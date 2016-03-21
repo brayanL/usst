f383171bf0c90e35ca28ef62de85a87136b7706b
Brayan Loayza <brayan@brayanl.brayan.com>
2016-03-21 02:19:31 -0500

    Cambios en Formulario de Evaluacion de Riesgo

--- apps/control/views.py
+++ apps/control/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 import json
-from .forms import *
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.db import transaction
+from django.contrib import messages
 
-from .models import factor_riesgo
+from .forms import *
+from .models import factor_riesgo, evaluacion_riesgo, peligro_evaluacion
 
 # Create your views here.
 def new_eval_riesgo(request):
+    if request.method == "POST":
+        try:
+            with transaction.atomic():
+                mfer = EvalRiesgoForm(request.POST)
+                if mfer.is_valid():
+                    er =mfer.save(commit=False)
+                    er.usuario = request.user
+                    er.save()
+                    messages.info(request, "Se ha guardado con éxito el nuevo registro.")
+                    return redirect("/eval_riesgo/")
+                else:
+                    messages.info(request, "Error al Guardar.")
+                #num_filas = request.POST['num_filas']
+
+        except Exception as error:
+            print(error)
+            transaction.rollback()
+            messages.error(request, ("Comuniquese con Soporte técnico. "+ str(error)))
+            return redirect('/eval_riesgo/')
+
     form = EvalRiesgoForm()
     return render(request, "new_eval_riesgo.html", {"collapse_er": "in", "active_n": "active", "form": form})
 
